3265dbd923fdea96a6c3f8d4e4c729e4d351dd00
prathibhanakka <30991469+prathibhanakka@users.noreply.github.com>
2017-08-15 19:48:33 -0400

    Add files via upload

---
+++ user-search/README.md
@@ -0,0 +1,2 @@
+# user-security
+This is simple introduction of how to setup any simple Spring Boot Project and Spring Data JPA
---
+++ user-search/src/main/java/com/gp/usersearch/UserSearchBootStrap.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.gp.usersearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ganeshp
+ *
+ */
+@SpringBootApplication
+public class UserSearchBootStrap {
+
+	/**
+	 * 
+	 */
+	public UserSearchBootStrap() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(UserSearchBootStrap.class, args);
+
+	}
+
+}
